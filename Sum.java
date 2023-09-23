12111679e5ead6d54235490debcd980b92a380fc
Garv Tandon <garvtandon04@gmail.com>
2023-09-23 17:37:00 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        int a,b;
+        int c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        a=sc.nextInt();
+        System.out.println("Enter the value of b:");
+        b=sc.nextInt();
+        c=a+b;
+        System.out.print("Sum is:"+c);
+
+    }
+}
