b4ad3ceb8c0437c31be2eb8835cdf6854061d0cd
Garv Tandon <garvtandon04@gmail.com>
2023-09-23 17:51:22 +0530

    Second Commit

---
+++ Diff.java
@@ -0,0 +1,10 @@
+public class Diff {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+        int c=a-b;
+        System.out.print("Diff is:"+c);
+    }
+    
+}
